f180a950af2aa672642a4d414a79df2ad655b11b
François Leurent <fleurent@ivsweb.com>
2018-09-04 09:35:01 +0000

    With travis env

--- test/index.js
+++ test/index.js
@@ -16,7 +16,18 @@ describe("initial test suite", function() {
 
   var ctx;
   before("should check for proper credentials", async () => {
-    ctx = await OVHContext.build(require('./credentials.json'));
+    var creds;
+    if(process.env['OS_USERNAME'])
+      creds = {
+        "username": process.env['OS_USERNAME'],
+        "password": process.env['OS_PASSWORD'],
+        "tenantId": process.env['OS_TENANT_ID'],
+        "region"  : process.env['OS_REGION_NAME']
+      };
+    else
+      creds = require('./credentials.json');
+
+    ctx = await OVHContext.build(creds);
     console.log("Context is ready");
   });
 
